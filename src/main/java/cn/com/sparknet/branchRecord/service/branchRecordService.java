81eaaa90395dbf40a25b0164e650160b8f3b5814
jerry <hanyuse@yeah.net>
2018-09-20 17:39:01 +0800

    init
    init

---
+++ src/main/java/cn/com/sparknet/branchRecord/service/branchRecordService.java
@@ -0,0 +1,5 @@
+package cn.com.sparknet.branchRecord.service;
+
+public class branchRecordService {
+
+}
---
+++ src/main/java/cn/com/sparknet/branchRecord/servlet/BranchRecord.java
@@ -0,0 +1,10 @@
+package cn.com.sparknet.branchRecord.servlet;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/api")
+public class BranchRecord {
+
+}
