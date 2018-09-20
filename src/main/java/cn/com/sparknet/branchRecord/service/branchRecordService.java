aa518b99b5b79f226d9b45ee94624650ab1b14de
jerry <hanyuse@yeah.net>
2018-09-20 18:11:42 +0800

    init
    init

--- src/main/java/cn/com/sparknet/branchRecord/service/branchRecordService.java
+++ src/main/java/cn/com/sparknet/branchRecord/service/branchRecordService.java
@@ -1,5 +1,71 @@
 package cn.com.sparknet.branchRecord.service;
 
-public class branchRecordService {
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Service;
+
+import net.sf.json.JSONObject;
+
+@Service
+public class BranchRecordService {
+	private static final Logger logger = LogManager.getLogger(BranchRecordService.class);
+	
+	public JSONObject queryptent(String path,JSONObject object){
+		JSONObject result = new JSONObject();
+		return result;
+	}
+	
+	/**
+	 * 发送http请求公共方法
+	 * @param path
+	 * @param object
+	 * @return
+	 */
+	public JSONObject sendHttp(String path,JSONObject object){
+		JSONObject result = new JSONObject();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		return result;
+	}
+	
+	/**
+	 * 读取资源配置文件
+	 * @return
+	 */
+	public  Map<String,Object> getProperties(){
+		InputStream in = null;
+		Properties properties = new Properties();
+		Map<String,Object> map = new HashMap<String,Object>();
+		try{
+			in = BranchRecordService.class.getClassLoader().getResourceAsStream("config.properties");
+			properties.load(in);
+			map.put("userName", properties.getProperty("userName"));
+			map.put("password", properties.getProperty("password"));
+			map.put("host", properties.getProperty("host"));
+		}catch(Exception e){
+			e.printStackTrace();
+		}finally{
+			if(in!=null){
+				try {
+					in.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return map;
+	}
 }
--- src/main/java/cn/com/sparknet/branchRecord/servlet/BranchRecord.java
+++ src/main/java/cn/com/sparknet/branchRecord/servlet/BranchRecord.java
@@ -1,10 +1,41 @@
 package cn.com.sparknet.branchRecord.servlet;
 
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import cn.com.sparknet.branchRecord.service.BranchRecordService;
+import net.sf.json.JSONObject;
 
 @Controller
 @RequestMapping(value = "/api")
 public class BranchRecord {
 
+	@Resource
+	private BranchRecordService branchReacrdService;
+
+	@ResponseBody
+	@RequestMapping(params = "/branch/queryptent", method = RequestMethod.POST)
+	public JSONObject queryptent(@RequestBody String json, HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
+		JSONObject object = JSONObject.fromObject(json);
+		JSONObject result = new JSONObject();
+		String entName = object.getString("ENTNAME");
+		String uniScid = object.getString("UNISCID");
+		if(entName==null || uniScid== null || entName.trim().equals("") || uniScid.trim().equals("")){
+			result.put("success",false);
+			result.put("data","母公司企业名称和母公司统一社会信用代码参数不能为空");
+			response.setStatus(406);
+			return result;
+		}
+		object.put("searchPath", "/branch/queryptent");
+		result = branchReacrdService.queryptent(object);
+		return result;
+	}
 }
